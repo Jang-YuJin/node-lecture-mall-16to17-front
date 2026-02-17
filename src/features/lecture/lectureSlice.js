0b364a23422da709e63189536c656e5839e69fdb
Jang-YuJin <jangyj0912@naver.com>
2026-02-17 18:46:44 +0900

    강의 등록 시 등록 완료하고 목록 업데이트 할 때 userId 가져갈 수 있도록 수정

--- src/features/lecture/lectureSlice.js
+++ src/features/lecture/lectureSlice.js
@@ -39,7 +39,7 @@ export const getLectureDetail = createAsyncThunk(
 
 export const createLecture = createAsyncThunk(
   "lectures/createLecture",
-  async (formData, { dispatch, rejectWithValue }) => {
+  async (formData, { dispatch, getState, rejectWithValue }) => {
     try {
       const response = await api.post('/lecture', formData);
 
@@ -48,7 +48,8 @@ export const createLecture = createAsyncThunk(
       }
 
       dispatch(showToastMessage({message: '강의 등록 완료', status: 'success'}));
-      dispatch(getLectureList({page: 1}));
+      const userId = getState()?.user?.user?._id;
+      dispatch(getLectureList({ page: 1, userId }));
 
       return response.data.data;
     } catch (error) {
